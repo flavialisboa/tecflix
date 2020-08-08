75b71acc4f42b6acf79257457c7509ead1add828
Flavia Lisboa <flavialisboa5@gmmail.com>
2020-08-08 10:27:54 -0300

    Update heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const jsonServer = require('json-server');
 
 const server = jsonServer.create();
@@ -9,5 +10,6 @@ const port = process.env.PORT || 8080;
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`JSON Server is running in ${port}`);
 });
